fefadea040ee5430d724f72c24da06b516ac6aa7
melissamariano <melissams538@gmail.com>
2023-04-02 04:07:48 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1=31
+let num2=2
+let multiply = num1*num2;
 
-// Write your code here
+{let random = 10
+    Math.floor(Math.random() * 10) + 1;}
+let num3 = 8
+let num4 = 2
+let mod = 8/2
+let max = (5,10,15,20);
